82b94ce72cfcb8c3142a59b21b0ad8bebe63f7c6
pcfreak9000 <roman.borris@gmail.com>
2025-06-11 17:13:59 +0200

    testing

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ gamma = 1.8
 afe = 1
 logxi = 1.
 ecut = 300
-incl = 10
+incl = 0
 
 spin = 0.0
 a13 = 0
@@ -23,8 +23,8 @@ epsi3 = 0
 a52 = 0
 alpha = -3
 
-Nr = 2000 #1000
-Nph = 800 #400
+Nr = 200 #1000
+Nph = 80 #400
 Rmax = 80
 rstep  = np.e**((np.log(Rmax)-np.log(np.cos(np.pi*incl/180)))/Nr);
 pstep  = 2*np.pi/Nph;
