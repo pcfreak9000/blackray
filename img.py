fixed gfactor calculation

img.py
@@ -40,9 +40,9 @@ def main(input_file, output_image, size=256, use_labels=False):
         }
 
         # Prepare RGB channels
-        grid_r = griddata((x_norm, y_norm), [label_colors[l][0] for l in label_raw], (grid_x, grid_y), method='nearest', fill_value=0)
-        grid_g = griddata((x_norm, y_norm), [label_colors[l][1] for l in label_raw], (grid_x, grid_y), method='nearest', fill_value=0)
-        grid_b = griddata((x_norm, y_norm), [label_colors[l][2] for l in label_raw], (grid_x, grid_y), method='nearest', fill_value=0)
+        grid_r = griddata((x_norm, 1.0-y_norm), [label_colors[l][0] for l in label_raw], (grid_x, grid_y), method='nearest', fill_value=0)
+        grid_g = griddata((x_norm, 1.0-y_norm), [label_colors[l][1] for l in label_raw], (grid_x, grid_y), method='nearest', fill_value=0)
+        grid_b = griddata((x_norm, 1.0-y_norm), [label_colors[l][2] for l in label_raw], (grid_x, grid_y), method='nearest', fill_value=0)
 
         # Stack to RGB image
         rgb_image = np.stack([grid_r, grid_g, grid_b], axis=2)
@@ -51,10 +51,11 @@ def main(input_file, output_image, size=256, use_labels=False):
     else:
         # Grayscale mode: 1 - normalized(data)
         d_inverted = normalize_column(d_raw)
-        grid_d = griddata((x_norm, y_norm), d_inverted, (grid_x, grid_y), method='nearest', fill_value=1.0)
+        grid_d = griddata((x_norm, 1.0-y_norm), d_inverted, (grid_x, grid_y), method='nearest', fill_value=1.0)
         plt.imsave(output_image, grid_d, cmap='gray', vmin=0, vmax=1)
 
 if __name__ == "__main__":
     # Set `use_labels` to True to enable color labeling
-    main("output.txt", "output.png", size=1024, use_labels=False)
+    main("output.txt", "outputg.png", size=1024, use_labels=False)
+    main("output.txt", "outputi.png", size=1024, use_labels=True)
 

run.py
@@ -14,17 +14,17 @@ gamma = 1.8
 afe = 1
 logxi = 1.
 ecut = 300
-incl = -70.0
+incl = 70.0
 
-spin = 0.998
+spin = 0.0
 a13 = 0
 a22 = 0
 epsi3 = 0
 a52 = 0
 alpha = -3
 
-Nr = 200 #1000
-Nph = 80 #400
+Nr = 100 #1000
+Nph = 40 #400
 Rmax = 80
 rstep  = np.e**((np.log(Rmax)-np.log(np.cos(np.pi*incl/180)))/Nr);
 pstep  = 2*np.pi/Nph;