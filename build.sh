3a942a0e4761faed0cf8a7db8b43db8ba49f6cb0
pcfreak9000 <roman.borris@gmail.com>
2025-11-21 16:22:28 +0100

    build improved

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ if [ "$BINAC2" ]; then
     module load devel/cmake/3.29
 fi
 
-cmake -B build -DCMAKE_BUILD_TYPE=Debug
-mkdir -p data
-mkdir -p ironline_data
-mkdir -p output
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+#mkdir -p data
+#mkdir -p ironline_data
+#mkdir -p output
 
 cd build
 make
